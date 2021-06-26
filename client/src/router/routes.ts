a8dc7a2c7a8b49299c5857a309953316238ab6ad
_nderscore <_@nderscore.com>
2021-06-26 02:12:32 -0400

    split paths out of routes list

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -2,12 +2,13 @@ import type { RouteProps } from 'react-router-dom';
 import { lazy } from 'react';
 
 import NotFound from '~/pages/NotFound';
+import { paths } from '~/router/paths';
 
 export const routes: RouteProps[] = [
   {
     component: lazy(() => import('~/pages/Home')),
     exact: true,
-    path: '/',
+    path: paths.home,
   },
   {
     component: NotFound,
